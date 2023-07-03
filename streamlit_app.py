9ea528c61960e1b0761248339eb0a27755a1d7e6
Vijay <46001608+vijay99227@users.noreply.github.com>
2023-07-03 13:59:30 +0530

    Update streamlit_app.py
    now fruits can be added by index/name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#pick the fruit to include
+streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
